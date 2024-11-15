d6152cb41e222ae6989fd08e26e82d30f6c94e91
rtthoma3 <rtthoma3@ncsu.edu>
2024-11-14 23:41:55 -0500

    Added get group(s) api routes

--- backend/src/groups/routes.py
+++ backend/src/groups/routes.py
@@ -12,6 +12,47 @@ except ImportError:
 group_bp = Blueprint('group_bp', __name__)
 db = firebase.database()
 
+@group_bp.route('/group/<id>', methods=['GET'])
+@cross_origin(supports_credentials=True)
+def getGroup(id):
+    '''This method fetches a specific group by its ID.'''
+    try:
+        group = db.child("group").child(id).get()
+        return jsonify(
+            group=group.val(),
+            message='Fetched group successfully',
+            status=200
+        ), 200
+    except Exception as e:
+        return jsonify(
+            group={},
+            message=f"An error occurred: {e}",
+            status=400
+        ), 400
+    
+@group_bp.route('/group/all', methods=['GET'])
+@cross_origin(supports_credentials=True)
+def getdecks():
+    '''Fetch all groups that a given user is a member of.'''
+    args = request.args
+    localId = args.get('localId')
+    
+    try:
+        groups = []
+        if localId:
+            allGroups = db.child("group").get()
+            for grp in allGroups.each():
+                obj = grp.val()
+                if localId in obj['members']:
+                    obj['id'] = grp.key()
+                    groups.append(obj)
+        else:
+            raise Exception("A valid user ID must be provided.")
+
+        return jsonify(groups=groups, message='Fetching groups successfully', status=200), 200
+    except Exception as e:
+        return jsonify(decks=[], message=f"An error occurred {e}", status=400), 400
+
 @group_bp.route('/group/create', methods=['POST'])
 @cross_origin(supports_credentials=True)
 def create():
